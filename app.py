f00be122a0f4ec2682524ae82aaa50b3ef952824
borvath <borvathcs@gmail.com>
2024-03-01 17:19:55 -0500

    Connected MySQL db to application for login page and registration

--- app.py
+++ app.py
@@ -1,20 +1,80 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
+from forms import LoginForm, RegisterForm
+from flask_bcrypt import Bcrypt
+from os import environ
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'QuizMaker'
+app.config['MYSQL_PASSWORD'] = f'{environ['MYSQL_PASS']}'
+app.config['MYSQL_DB'] = 'quizmaker'
+app.config['SECRET_KEY'] = 'Hey'
 
 mysql = MySQL(app)
+bcrypt = Bcrypt(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """
     - Should redirect user to login page if not logged in
     - If logged in should direct user to home page 'index.html'
     """
-    return render_template('index.html')
+    if 'user' in session:
+        return render_template('index.html')
+    else:
+        return render_template('login.html', form=LoginForm())
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html", title='Login', form=LoginForm())
+    else:
+        cursor = mysql.connect.cursor()
+        cursor.execute("SELECT id, password FROM user WHERE username=%s AND type=%s;",
+                       (request.form['username'], request.form['type']))
+        data = cursor.fetchone()
+        print(data)
+        if data is None:
+            session['login_error'] = "User not found"
+            return render_template("login.html", title='Login', form=LoginForm())
+        else:
+            if bcrypt.check_password_hash(data[-1], request.form['password']):
+                session['user'] = request.form['username']
+                return redirect(url_for('index'))
+            else:
+                session['login_error'] = "Password not valid"
+                return render_template("login.html", title='Login', form=LoginForm())
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html', form=RegisterForm())
+    else:
+        conn = mysql.connect
+        cursor = conn.cursor()
+        form = request.form
+        try:
+            hashed_password = bcrypt.generate_password_hash(form['password']).decode('utf-8')
+            query = "INSERT INTO user(first_name, last_name, username, password, type) VALUES (%s, %s, %s, %s, %s)"
+            values = (form['first_name'], form['last_name'], form['username'], hashed_password, form['type'])
+            cursor.execute(query, values)
+            conn.commit()
+            cursor.close()
+            conn.close()
+        except Exception as e:
+            print(f"{type(e)}: {e}")
+            session['register_error'] = e
+            cursor.close()
+            conn.close()
+            return render_template('register.html', form=RegisterForm())
+        session['user'] = request.form['username']
+        return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,60 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+
+PASSWORD_MIN_SIZE = 6
+PASSWORD_MAX_SIZE = 255
+
+USERNAME_MIN_SIZE = 6
+USERNAME_MAX_SIZE = 100
+
+FIRST_NAME_MAX_SIZE = 100
+LAST_NAME_MAX_SIZE = 100
+
+
+class RegisterForm(FlaskForm):
+    type = SelectField(
+        label='type',
+        validators=[InputRequired()],
+        choices=[('student', 'student'), ('teacher', 'teacher'), ('admin', 'admin')]
+    )
+    first_name = StringField(
+        label='first_name',
+        validators=[InputRequired(), Length(min=0, max=FIRST_NAME_MAX_SIZE)],
+        render_kw={"placeholder": "John"}
+    )
+    last_name = StringField(
+        label='last_name',
+        validators=[InputRequired(), Length(min=0, max=LAST_NAME_MAX_SIZE)],
+        render_kw={"placeholder": "Smith"}
+    )
+    username = StringField(
+        label='username',
+        validators=[InputRequired(), Length(min=USERNAME_MIN_SIZE, max=USERNAME_MAX_SIZE)],
+        render_kw={"placeholder": "Username"}
+    )
+    password = PasswordField(
+        label='password',
+        validators=[InputRequired(), Length(min=PASSWORD_MIN_SIZE, max=PASSWORD_MAX_SIZE)],
+        render_kw={"placeholder": "Password"}
+    )
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    type = SelectField(
+        label='type',
+        validators=[InputRequired()],
+        choices=[('student', 'student'), ('teacher', 'teacher'), ('admin', 'admin')]
+    )
+    username = StringField(
+        label='username',
+        validators=[InputRequired(), Length(min=USERNAME_MIN_SIZE, max=USERNAME_MAX_SIZE)],
+        render_kw={"placeholder": "Username"}
+    )
+    password = PasswordField(
+        label="password",
+        validators=[InputRequired(), Length(min=PASSWORD_MIN_SIZE, max=PASSWORD_MAX_SIZE)],
+        render_kw={"placeholder": "Password"}
+    )
+    submit = SubmitField("Login")
