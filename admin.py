56aa204253b00da0521eae8c72e3b9321c447482
Brian Nelson <122384363+bnnelson1@users.noreply.github.com>
2024-04-05 01:35:01 -0400

    Added initial work towards implementing user management for admins.
    - Added admin.py which contains the bare minimum for returning a page with a list of users. (utilizes flask's blueprint system)
    
    - Added view_users.html and table.css (potentially temporary css file/name for styling the user table).

---
+++ admin.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template
+from database import execute_select_statement
+
+admin_bp = Blueprint('admin', __name__)
+
+
+# TODO: Authentication
+@admin_bp.route("/viewusers", methods=['GET', 'POST'])
+def view_users():
+    """Renders page where admins can view users that exist in the database."""
+    query = "SELECT id, username, last_name, first_name, type FROM user;"
+    result = execute_select_statement(query)
+    headings = ["ID", "Username", "Last Name", "First Name", "Type"]
+    return render_template('admin_pages/view_users.html', users=result, headings=headings)
