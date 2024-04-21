d47876e7b8c465e2cbc6c52e250a94cbcb450de1
AditiAnan <aditianandjiwala10@gmail.com>
2024-04-21 17:13:40 -0400

    linking view classes to the class page

--- app.py
+++ app.py
@@ -67,21 +67,31 @@ def viewRatings():
 def take_quiz():
     return render_template('takeQuiz.html')
 
+classes = [
+    {"name": "COP 3363 Introduction to C++", "link": "cop3363"},
+    {"name": "COP 3330 Data Structures, Algorithms, and Generic Programming I", "link": "cop3330"},
+    {"name": "CDA 3100 Computer Organization I", "link": "cda3100"},
+    {"name": "CEN 4020 Software Engineering I", "link": "cen4020"},
+    {"name": "CEN 4090L Software Engineering Capstone", "link": "cen4090l"},
+    {"name": "CIS 3250 Ethics in Computer Science", "link": "cis3250"},
+    {"name": "COP 4610 Operating Systems & Concurrent Programming", "link": "cop4610"},
+    {"name": "COP 4530 Data Structures, Algorithms, and Generic Programming II", "link": "cop4530"},
+    {"name": "COP 4521 Secure Parallel & Distributed Programming w/ Python", "link": "cop4521"},
+    {"name": "COT 4420 Theory of Computation", "link": "cot4420"}
+]
+
 @app.route('/class')
-def showClasses():
-    classes = [
-        {"name": "COP 3363 Introduction to C++", "link": "/cop3363"},
-        {"name": "COP 3330 Data Structures, Algorithms, and Generic Programming I", "link": "/cop3330"},
-        {"name": "CDA 3100 Computer Organization I", "link": "/cda3100"},
-        {"name": "CEN 4020 Software Engineering I", "link": "/cen4020"},
-        {"name": "CEN 4090L Software Engineering Capstone", "link": "/cen4090l"},
-        {"name": "CIS 3250 Ethics in Computer Science", "link": "/cis3250"},
-        {"name": "COP 4610 Operating Systems & Concurrent Programming", "link": "/cop4610"},
-        {"name": "COP 4530 Data Structures, Algorithms, and Generic Programming II", "link": "/cop4530"},
-        {"name": "COP 4521 Secure Parallel & Distributed Programming w/ Python", "link": "/cop4521"},
-        {"name": "COT 4420 Theory of Computation", "link": "/cot4420"}
-    ]
-    return render_template('classes.html', classes=classes)
+def show_classes():
+    return render_template('ViewClasses.html', classes=classes)
+
+@app.route('/class/<class_name>')
+def class_details(class_name):
+    class_data = next((item for item in classes if item["link"] == class_name), None)
+    if class_data:
+        return render_template('class_details.html', class_name=class_data["name"])
+    else:
+        return "Class not found", 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
