e804c10729a63c48be5f1f1fa89a54cd3d28ea26
Brian Nelson <122384363+bnnelson1@users.noreply.github.com>
2024-04-20 21:51:38 -0400

    Added authentication to admin viewusers route.

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from database import execute_select_statement
+from auth import check_user_permissions
 
 admin_bp = Blueprint('admin', __name__)
 
@@ -8,7 +9,9 @@ admin_bp = Blueprint('admin', __name__)
 @admin_bp.route("/viewusers", methods=['GET', 'POST'])
 def view_users():
     """Renders page where admins can view users that exist in the database."""
-    query = "SELECT id, username, last_name, first_name, type FROM user;"
-    result = execute_select_statement(query, None)
-    headings = ["ID", "Username", "Last Name", "First Name", "Type"]
-    return render_template('admin_pages/view_users.html', users=result, headings=headings)
+    if check_user_permissions("admin"):
+        query = "SELECT id, username, last_name, first_name, type FROM user;"
+        result = execute_select_statement(query, None)
+        headings = ["ID", "Username", "Last Name", "First Name", "Type"]
+        return render_template('admin_pages/view_users.html', users=result, headings=headings)
+    abort(403)
