ca7f68fd7d20b6e694d7d5a66360189ec718f4bd
Brian Nelson <122384363+bnnelson1@users.noreply.github.com>
2024-04-23 21:52:12 -0400

    Added some documentation to logutil.

--- logutil.py
+++ logutil.py
@@ -1,8 +1,9 @@
 from database import execute_select_statement, execute_non_select_statement
 
 
-def log_login(username):
+def log_login(username) -> int:
     """Inserts a row into the login_log table to log user logins"""
+
     query = "SELECT id FROM user WHERE username=%s;"
     values = (username,)
     user = execute_select_statement(query, values, 1)
@@ -14,7 +15,11 @@ def log_login(username):
     return False
 
 
-def get_all_quiz_creation_times(user_info=False):
+def get_all_quiz_creation_times(user_info: bool = False) -> list[dict] | None:
+    """Returns creation time data for all quizzes.
+    If user_info is False, selected data is quiz id, name, and creation time.
+    If user_info is True, selected data also includes author id and username."""
+
     if user_info:
         query = ("SELECT quiz.id, quiz.author_id, quiz.name, user.username, quiz.creation_time"
                  " FROM quiz INNER JOIN user WHERE quiz.author_id=user.id"
@@ -25,12 +30,16 @@ def get_all_quiz_creation_times(user_info=False):
     return quizzes
 
 
-def get_quiz_creation_time(quiz_id, user_info=False):
+def get_quiz_creation_time(quiz_id: int, user_info: bool = False) -> dict | None:
+    """Returns creation time data for a single quiz.
+    If user_info is False, selected data is quiz id, name, and creation time.
+    If user_info is True, selected data also includes author id and username."""
+
     if user_info:
         query = ("SELECT quiz.id, quiz.name, user.id, user.username, quiz.creation_time"
                  " FROM quiz INNER JOIN user WHERE quiz.id=%s AND quiz.author_id=user.id;")
     else:
         query = "SELECT quiz.id, quiz.name, quiz.creation_time FROM quiz WHERE quiz.id=%s;"
     values = (quiz_id,)
-    quizzes = execute_select_statement(query, values)
-    return quizzes
+    quiz = execute_select_statement(query, values)
+    return quiz
